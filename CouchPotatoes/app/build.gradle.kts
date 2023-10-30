f651bea0fd4f23fa8be7482ea6070a42f796700b
Luke Li <lukewli@ucsb.edu>
2023-10-29 22:30:39 -0700

    lli - Added custom buttons to replace old placeholders

--- CouchPotatoes/app/build.gradle.kts
+++ CouchPotatoes/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.couchpotatoes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.couchpotatoes"
@@ -45,7 +45,8 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.0-beta02")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
+
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
