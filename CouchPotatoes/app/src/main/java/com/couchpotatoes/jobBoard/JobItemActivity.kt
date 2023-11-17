836c6f056d0ac7d87bc82e8d0ded2370e47ce496
wesleykam <wesley_kam@ucsb.edu>
2023-11-17 15:37:08 -0800

    remove unused imports

--- CouchPotatoes/app/src/main/java/com/couchpotatoes/jobBoard/JobItemActivity.kt
+++ CouchPotatoes/app/src/main/java/com/couchpotatoes/jobBoard/JobItemActivity.kt
@@ -4,13 +4,9 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.AlignmentSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
